4cf409d7d002aef44e42cfec0fa6ca4198bf7b62
demola07 <45898468+demola07@users.noreply.github.com>
2020-05-16 10:32:19 +0100

    Challenge complete

--- server.js
+++ server.js
@@ -3,34 +3,65 @@
 var express = require('express');
 var mongo = require('mongodb');
 var mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const dns = require('dns');
 
 var cors = require('cors');
 
 var app = express();
 
-// Basic Configuration 
+// Basic Configuration
 var port = process.env.PORT || 3000;
 
-/** this project needs a db !! **/ 
+/** this project needs a db !! **/
+
 // mongoose.connect(process.env.DB_URI);
 
 app.use(cors());
 
-/** this project needs to parse POST bodies **/
-// you should mount the body-parser here
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use('/public', express.static(process.cwd() + '/public'));
 
-app.get('/', function(req, res){
+app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-  
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+const links = [];
+let id = 0;
+
+// your first API endpoint...
+app.post('/api/shorturl/new', function (req, res) {
+  const { url } = req.body;
+
+  const noHTTPSUrl = url.replace(/^https?:\/\//, '');
+
+  dns.lookup(noHTTPSUrl, (err) => {
+    if (err) {
+      return res.json({ error: 'invalid URL' });
+    } else {
+      id++;
+      const newLink = {
+        original_url: url,
+        short_url: `${id}`,
+      };
+
+      links.push(newLink);
+      return res.json(newLink);
+    }
+  });
 });
 
+app.get('/api/shorturl/:id', (req, res) => {
+  const { id } = req.params;
+  const shortURL = links.find((link) => link.short_url === id);
+
+  if (shortURL) {
+    return res.redirect(shortURL.original_url);
+  } else {
+    return res.json({ error: 'No short url' });
+  }
+});
 
 app.listen(port, function () {
   console.log('Node.js listening ...');
